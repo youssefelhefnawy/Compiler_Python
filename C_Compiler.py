974185a07686b62981f00806bee10d191e1aed4b
youssefelhefnawy <youssefelhefnawy@gmail.com>
2020-03-12 17:49:43 +0200

    Final Version,,

--- C_Compiler.py
+++ C_Compiler.py
@@ -5,41 +5,100 @@ def ReadFile(FileName):
     FileInstance.close()
     return All_Lines
     
-def ProduceTokens(Statements):
+def swap(a,b):
+    temp=b
+    b=a
+    a=temp
+    return [a,b]
+def SeeMatches(TokenName,listofmatches):    
+    Matches=[]
+    for match in listofmatches:
+        Matches.append([TokenName,match])
 
-    return
-    
+    return Matches
+def SortMatches(Matches):
+    for i in range(0,len(Matches)):
+        for j in range(0,len(Matches)):  
+            if Matches[i][1].start()>Matches[j][1].start():
+                [Matches[i],Matches[j]]=swap(Matches[i],Matches[j])
+                
+def SetDirty(start,end,List):
+    for i in range(start,end):
+        List[i]=1
+    return List
 ListOfLines=ReadFile("Input.txt")
-List_2nd_of_Tokens=['INTEGRAL_LITERAL','FLOAT_LITERAL'
-                ,'STRING_LITERAL','CHAR_LITERAL',
-                'LEFT_CURLY_B','RIGHT-CURLY_B',
-                'LEFT_SQUARE_B','RIGHT_SQUARE_B',
-                'LEFT_ROUND_B','RIGHT_ROUND_B'
-                'COMMA','SEMICOLON','DOT''NOT'
-                'ASSIGN_OPERATOR','PREPROCESSOR'
-                'BACKWARD_SLASH','MINUS','PLUS'
-                'ASTERICK','DIVIDE','MOD',
-                'LESSTHAN','GREATERTHAN','LESS_EQ',
-                'GREAT_EQ','EQUAL','NOT_EQUAL','AND'
-                'OR','BITWISE_AND','BITWISE_OR',
-                'BITWISE_XOR','LEFT_SHIFT','RIGHT_SHIFT'
-                'BITWISE_NOT','MULTI_COMMENT','SINGLE_COMMENT'
-                ]
-Regex_2nd=['\d+','\d+(\.\d+)?',
+Regex_2nd={
+             r'/\*(\W|\w)*\*/':'MULTIPLE COMMENT',
+            r'//.*\n':'SINGLE COMMENT',
+            r'\"[^"]*\"':'STRING_LITERAL',
+            r'\b\d+(\.\d+)\b':'FLOAT_LITERAL',
+            r'\b\d+\b':'INTEGRAL_LITERAL',
+            r"\'[^']?\'":'CHAR_LITERAL',
+            r'{':'LEFT_CURLY_B' ,
+            r'}':'RIGHT-CURLY_B',
+            r'\[':'LEFT_SQUARE_B',
+            r'\]':'RIGHT_SQUARE_B',
+            r'\(':'LEFT_ROUND_B',
+            '\)':'RIGHT_ROUND_B',
+            ',':'COMMA',
+            ';':'SEMICOLON'
+            ,r'\.':'DOT'
+            
+            ,'#':'PREPROCESSOR'
+            ,r'\\':'BACKSLASH'
+            ,'-':'MINUS'
+            ,r'\+':'PLUS'
+            ,'r\*':'MULTIPLY'
+            ,r'/':'DIVIDE'
+            ,r'%':'MOD'
+            ,'<<':'LEFTSHIFT'
+            ,'>>':'RIGHTSHIFT'  
+            ,'<=':'LESS THAN OR EQUAL'
+            ,'>=':'GREATER THAN OR EQUAL'
+            ,'<':'LESSTHAN'
+            ,r'\]==\]':'EQUAL'
+           ,r'!=':'NOT EQUAL'
+           ,'!':'NOT',
+            r'=':'ASSIGNMENT',
+           '&&':'AND',
+            '>':'GREATER THAN',
+            r'\|\|':'OR'
+            ,r'&':'BITWISE AND'
+            ,'\|':'BITWISE OR',
+            r'\^':'BITIWSE XOR'   
+            ,'~':'BITWISE NOT'
+        }##Lessa fi mshakl
            
-         '\"[^\"]*\"',
-          
-         '\'[^\']?\'',
-           
-           '{','}',
-           
-           '\[',
-           '\]',
-           '\(','\)',','
-        ]
+    
 
-print(Regex.search('\"[^\"]*\"', " \"2312\" ").group(0))
-#Result=Regex.findall('\d+[\.\d+]?',"2.3121")
-#print(type(Result.group())) -- > Regex.Search
-#print(Result.group(0))
-#print(Result)
+WholeText=""
+for Line in ListOfLines:
+    WholeText=WholeText+Line
+MarkedChars=[]
+for i in range(0,len(WholeText)):
+    MarkedChars.append(0)    
+
+All_Matches=[]
+for pattern in Regex_2nd:
+    Result=Regex.finditer(pattern,WholeText)
+    Regex_Matches=[]
+    for match in Result:
+
+        if MarkedChars[match.start()]==0:
+            Regex_Matches.append([Regex_2nd[pattern],match])
+            MarkedChars=SetDirty(match.start(),match.end(),MarkedChars)
+        else:
+            continue
+            ##Do Nothing
+            
+    All_Matches=All_Matches+Regex_Matches
+                  
+SortMatches(All_Matches)
+All_Matches.reverse()
+
+for Match in All_Matches:
+    print(Match[0],"---->",Match[1].group()+"---------")
+dictio={'model':'ford'}
+
+###use r before string
+###use finditer
