0b3f1891c6cdc667d22c148497be220589287b69
YoussefFekky <yousseffekky@gmail.com>
2020-04-16 22:19:17 +0200

    Update Token.java

--- src/Token.java
+++ src/Token.java
@@ -1,4 +1,19 @@
 
 public class Token {
-
+	String TYPE;
+	String VALUE;
+	
+	public String getTYPE() {
+		return TYPE;
+	}
+	public void setTYPE(String type) {
+		TYPE = type;
+	}
+	public String getVALUE() {
+		return VALUE;
+	}
+	public void setVALUE(String value) {
+		VALUE = value;
+	}
+	
 }
