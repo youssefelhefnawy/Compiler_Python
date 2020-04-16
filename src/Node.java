42400ca15f62b4cc3b45952f98f58fdcc95f9ccd
YoussefFekky <yousseffekky@gmail.com>
2020-04-17 00:54:45 +0200

    Update Node.java

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,32 @@
+import java.util.ArrayList;
 
 public class Node {
+	String TYPE;
+	ArrayList<Node> ChildNodes = null;
+	
+	public Node(String type) {
+		TYPE = type;
+	}
+	
+	String getType() {
+		return TYPE;
+	}
 
+	public void addNode(String type) {
+		if(ChildNodes == null)
+			ChildNodes = new ArrayList<Node>();
+		ChildNodes.add(new Node(type));
+	}
+	
+	public void print() {
+		System.out.print("[ ");
+		for(Node node: ChildNodes) {
+			System.out.print(node.getType());
+		}
+		System.out.print(" ]");
+		for(Node node: ChildNodes) {
+			node.print();
+		}
+	}
+	
 }
